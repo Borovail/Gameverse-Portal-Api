9afc46677f4ad97a6a6a9ace8961daa1714bae07
Borovail <skorikdaniil17@gmail.com>
2024-03-21 13:29:57 +0200

    Cleaned up Program.cs
    code extracted from program.cs to extensions

---
+++ Back-End/Extensions/AuthenticationServiceExtensions.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+
+namespace Back_End.Extensions
+{
+    public static class AuthenticationServiceExtensions
+    {
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var authenticationBuilder = services.AddAuthenticationSchemes();
+
+            authenticationBuilder
+                .AddJwtBearerConfiguration(configuration)
+                .AddJwtBearerEventsConfiguration();
+
+            return services;
+        }
+
+        private static AuthenticationBuilder AddAuthenticationSchemes(this IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            });
+
+            return services.AddAuthentication();
+        }
+
+        private static AuthenticationBuilder AddJwtBearerConfiguration(this AuthenticationBuilder builder, IConfiguration configuration)
+        {
+            builder.AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"])),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = configuration["Jwt:Issuer"],
+                    ValidAudience = configuration["Jwt:Audience"],
+                };
+            });
+
+            return builder;
+        }
+
+
+        private static AuthenticationBuilder AddJwtBearerEventsConfiguration(this AuthenticationBuilder builder)
+        {
+            builder.AddJwtBearer(options =>
+            {
+                options.Events = new JwtBearerEvents
+                {
+                    OnChallenge = async context =>
+                    {
+                        context.HandleResponse();
+                        var result = new
+                        {
+                            Success = false,
+                            Message = "You are not authorized.",
+                            StatusCode = StatusCodes.Status401Unauthorized
+                        };
+                        var resultJson = JsonSerializer.Serialize(result);
+                        context.Response.ContentType = "application/json";
+                        await context.Response.WriteAsync(resultJson);
+                    },
+                    OnForbidden = async context =>
+                    {
+                        var result = new
+                        {
+                            Success = false,
+                            Message = "You do not have access to this resource.",
+                            StatusCode = StatusCodes.Status403Forbidden
+                        };
+                        var resultJson = JsonSerializer.Serialize(result);
+                        context.Response.ContentType = "application/json";
+                        await context.Response.WriteAsync(resultJson);
+                    }
+                };
+            });
+
+            return builder;
+        }
+
+
+
+
+    }
+}
--- Back-End/Program.cs
+++ Back-End/Program.cs
@@ -1,127 +1,52 @@
-using Microsoft.EntityFrameworkCore;
-using Back_End.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text.Json;
-using System.Security.Claims;
-using System.Text;
-using Microsoft.AspNetCore.Mvc;
 using Back_End.Middleware;
 using Back_End.Utils;
+using Back_End.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Registering custom JWT token service
 builder.Services.AddScoped<JwtTokenService>();
 
+// Adding custom service extensions to the container
+builder.Services
+    .AddJwtAuthentication(builder.Configuration) // Adding JWT authentication
+    .AddAuthorizationPolicies() // Adding custom authorization policies
+    .AddCorsPolicy() // Configuring CORS policy
+    .AddDbConfiguration(builder.Configuration) // Configuring DbContext
+    .AddIdentityConfiguration(); // Configuring Identity
 
-builder.Services.AddDbContext<ApiDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Setting up Identity
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-{
-    options.User.RequireUniqueEmail = true;
-})
-    .AddEntityFrameworkStores<ApiDbContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("UserAccess", policy => policy.RequireRole("User", "Developer", "Admin"));
-    options.AddPolicy("DeveloperAccess", policy => policy.RequireRole("Developer", "Admin"));
-    options.AddPolicy("AdminAccess", policy => policy.RequireRole("Admin"));
-});
-
-
-//Setting up JWT authentication
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        ValidateAudience = true,
-        ValidateIssuer = true,
-        ValidateLifetime = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
-        RoleClaimType = ClaimTypes.Role
-    };
-
-    options.Events = new JwtBearerEvents
-    {
-        OnChallenge = async context =>
-        {
-            //make correct message for unauthorized requests
-            context.HandleResponse();
-            context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-            var failure = context.AuthenticateFailure;
-            string resultJson;
-
-            if (failure != null)
-            {
-                resultJson = JsonSerializer.Serialize(failure.Message);
-                await Console.Out.WriteLineAsync(resultJson);  // replace this stuff with logger  when it will be extracted to extention
-            }
-
-            resultJson = JsonSerializer.Serialize("You are not authorized.");
-            await context.Response.WriteAsync(resultJson);
-        },
-        OnForbidden = async context =>
-        {
-            context.Response.StatusCode = StatusCodes.Status403Forbidden;
-            context.Response.ContentType = "application/json";
-            var resultJson = JsonSerializer.Serialize("You are not authorized to access this resource.");
-            await context.Response.WriteAsync(resultJson);
-        }
-    };
-});
-
-
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigin",
-        builder => builder.WithOrigins("https://localhost:5173") // Замените этим адресом ваш фронтенд
-        .AllowAnyMethod()
-        .AllowAnyHeader());
-});
-
-
+// Adding controllers to the service container
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+// Setting up Swagger for API documentation
 builder.Services.AddSwaggerGen();
-
-
+builder.Services.AddEndpointsApiExplorer(); // Required for Swagger
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline  .
+// Configuring the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
+    // Enabling Swagger in development environment
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseMiddleware<ExceptionHandlingMiddleware>();   
+// Exception handling middleware for global error handling
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
+// Applying CORS policy
 app.UseCors("AllowSpecificOrigin");
 
+// Enforcing HTTPS redirection
 app.UseHttpsRedirection();
 
+// Authentication & Authorization
 app.UseAuthentication();
-
 app.UseAuthorization();
 
+// Routing controllers
 app.MapControllers();
 
+// Starting the application
 app.Run();
